5c55963ab97bc1d950ddb593eb642d1815f6ed92
nicolifelix <nicolifelix@hotmail.com>
2021-09-09 08:45:33 -0300

    codigo swiper

--- assets/js/functions.js
+++ assets/js/functions.js
@@ -1,13 +1,15 @@
 const swiper = new Swiper('.swiper', {
-    // Optional parameters
+    slidesPerView: 1,
+    spaceBetween: 30,
     loop: true,
-
-    // If we need pagination
     pagination: {
-        el: '.swiper-pagination',
-        clickable: true,
+      el: ".swiper-pagination",
+      clickable: true,
     },
-    
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+    },
 
 
 });
